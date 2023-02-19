f3ce3e691878e936b30637981e2a16f632b75bd2
Adeleye Abdulbasit <adeleyeabdulbasit@gmail.com>
2023-02-18 21:46:07 -0800

    update

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-$(".testimonial_slider_area").owlCarousel({
-    autoplay:true,
-    slideSpeed:4000,
-    items:3,
-    nav:false,
-    navText: ['<i class ="fa fa-arrow-left"></i>', '<i class ="fa fa-arrow-right"></i>'],
-    margin:30,
-    dots:false,
-    responsive:{
-      375:{
-        items:1,
-        dots:true,
-      },
-      600:{
-        items:2
-      },
-      767:{
-        items:3
-      },
-      1000:{
-        items:3
-      }
-    }
+// $(".testimonial_slider_area").owlCarousel({
+//     autoplay:true,
+//     slideSpeed:4000,
+//     items:3,
+//     nav:false,
+//     navText: ['<i class ="fa fa-arrow-left"></i>', '<i class ="fa fa-arrow-right"></i>'],
+//     margin:30,
+//     dots:false,
+//     responsive:{
+//       375:{
+//         items:1,
+//         dots:true,
+//       },
+//       600:{
+//         items:2
+//       },
+//       767:{
+//         items:3
+//       },
+//       1000:{
+//         items:3
+//       }
+//     }
   
-  })
+//   })
 
-  const NAVTOGGLE = document.querySelector('.nav-toggle');
+ 
+
+const NAVTOGGLE = document.querySelector('.nav-toggle');
   const NAV = document.querySelector('.nav');
 
   NAVTOGGLE.addEventListener('click', () => {
